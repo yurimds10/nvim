f9e0fd8c24af9d8a739b11d5fc1116ca5f64c13e
yurimds10 <coder73854@proton.me>
2024-08-09 00:51:51 -0300

    :sparkles: feat: add telescope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,3 +41,9 @@ require("lazy").setup({
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
