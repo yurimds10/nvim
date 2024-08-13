a6c7a737e65d8cd7aa20c6ad1ceb59f060e2f71c
yurimds10 <coder73854@proton.me>
2024-08-13 14:17:11 -0300

    :sparkles: feat: add options config

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
 require("config.lazy")
+require("config.options")
 require("config.keymaps")
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.termguicolors = true
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
