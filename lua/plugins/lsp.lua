fada5f1d552d58ed22efd2febb9f7d9dddb517a3
yurimds10 <coder73854@proton.me>
2024-09-25 18:46:04 -0300

    :bug: fix: switch tsserver to ts_ls and add lsp servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,20 +9,32 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" }
+        ensure_installed = {
+          "bashls",
+          "clangd",
+          "html",
+          "cssls",
+          "lua_ls",
+          "ts_ls",
+          "tailwindcss"
+        }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
-    config = function ()
+    config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
+      lspconfig.bashls.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.tsserver.setup({capabilities = capabilities })
-      lspconfig.html.setup({capabilities = capabilities})
+      lspconfig.ts_ls.setup({capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
