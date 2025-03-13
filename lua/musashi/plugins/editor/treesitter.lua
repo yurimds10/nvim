b03db5898cc3b5b24014a34f3f384620c3a425bf
Yuri Mendes <coder73854@proton.me>
2025-03-13 12:14:06 -0300

    :sparkles: feat: added treesitter plugin

---
+++ lua/musashi/plugins/editor/treesitter.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      highlight = { enable = true },
+    })
+  end
+}
