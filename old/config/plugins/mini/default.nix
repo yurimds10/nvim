8cbef8e79c0903607000ca63d39ba6575386b1f0
Yuri Mendes <coder73854@proton.me>
2025-03-13 12:10:10 -0300

    :fire: move nixvim to lazy.nvim

---
+++ old/config/plugins/mini/default.nix
@@ -0,0 +1,15 @@
+{
+  plugins.mini = {
+    enable = true;
+    mockDevIcons = true;
+  };
+
+  imports = [
+    ./align.nix
+    ./diff.nix
+    ./hipatterns.nix
+    ./icons.nix
+    #./indentscope.nix
+    ./pairs.nix
+  ];
+}
