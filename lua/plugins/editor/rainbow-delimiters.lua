3c4085cb3d5416c85da497f1237c7ad6036712cd
Yuri Mendes <coder73854@proton.me>
2025-04-21 18:12:34 -0300

    :sparkles: feat: add rainbow-delimiters plugin

---
+++ lua/plugins/editor/rainbow-delimiters.lua
@@ -0,0 +1,25 @@
+return {
+  "hiphish/rainbow-delimiters.nvim",
+
+  config = function()
+    require("rainbow-delimiters.setup").setup({
+      strategy = {
+        [""] = "rainbow-delimiters.strategy.global",
+        vim = "rainbow-delimiters.strategy.local",
+      },
+      query = {
+        [""] = "rainbow-delimiters",
+        lua = "rainbow-blocks",
+      },
+      highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+      },
+    })
+  end,
+}
